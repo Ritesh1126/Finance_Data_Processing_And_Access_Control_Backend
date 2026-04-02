430424af99150002155f938da10c3de703dc0a72
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-04-02 23:59:00 +0530

    done

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./src/app");  // we have import the app from app.js file to use in this server.js file
-                                    // we have set the port number to 3000 or we can use the port number from environment variable
+                                  // we have set the port number to 3000 or we can use the port number from environment variable
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
