638bf523802fafa1570d8c2b73ee2898cedf972d
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-04-05 23:41:41 +0530

    server.js edited

--- server.js
+++ server.js
@@ -3,19 +3,8 @@ require('dotenv').config(); // Must
 const connectToDB = require("mongoose");
 const app = require("./src/app");
 
-// MongoDB connection
-connectToDB.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("MongoDB Connected");
-
+connectToDB()
     // Start server only after DB connects
     app.listen(3000, () => {
         console.log("Server is running on port 3000");
     });
-})
-.catch((err) => {
-    console.log("DB Connection Error:", err);
-    process.exit(1); // IF DB connection fails , then it exits the process
-});
-
-module.export = connectToDB;
