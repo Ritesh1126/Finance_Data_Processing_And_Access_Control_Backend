1d260f1cf797cca18799de16a811af49a901cf52
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-04-07 03:17:13 +0530

    controller implementation

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+const  userModel = require("../models/user.models")
+
+
+/*
+-- user register controller
+-- POST /api/auth/register
+
+*/
+function userRegisterController(req, res){
+    const {email, password, name} = req.body;
+
+}
+
+moudule.exports = {
+    userRegisterController
+}
