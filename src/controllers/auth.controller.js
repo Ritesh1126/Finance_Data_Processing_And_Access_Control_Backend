4336588bb4ffcb73f5592c2ceca0745c4847b817
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-04-07 03:30:21 +0530

    json required

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,31 @@
 const  userModel = require("../models/user.models")
-
+const jwt = require("jsonwebtoken")
 
 /*
 -- user register controller
 -- POST /api/auth/register
 
 */
-function userRegisterController(req, res){
+async function userRegisterController(req, res){
     const {email, password, name} = req.body;
 
+    const isExists = await userModel.findOne ({
+        email:email
+    })
+
+    if (isExists)
+    {
+        return res.status(422).json({
+            message : "User Already Exists with this email",
+            status: "failed"
+        })
+    }
+
+    const user = await userModel.create({
+        email, password, name
+    })
+
+    const token = jst.sign()
 }
 
 moudule.exports = {
