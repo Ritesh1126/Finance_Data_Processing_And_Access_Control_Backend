ce823347556bf6334d41346f0ab19a1ef7650b8a
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-04-07 03:17:38 +0530

    controler json file and routes added

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const authRouter = require("./routes/auth.routes");
 
 const app = express();  // we have export this app to use in other file like server.js 
 
+app.use(express.json());
+
 app.use("/api/auth", authRouter);
 
 
