a8a7dc73befbc16d72e2efced8206468e3f52e53
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-04-07 03:01:51 +0530

    router added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
+const authRouter = require("./routes/auth.routes");
 
 const app = express();  // we have export this app to use in other file like server.js 
+
+app.use("/api/auth", authRouter);
+
 
 module.exports = app; // exporting the app 
