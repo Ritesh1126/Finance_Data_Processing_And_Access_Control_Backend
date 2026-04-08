8b9b9f1f0a88a90ee02a3b89b46f8a940a5ced57
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-04-08 23:55:07 +0530

    server js changes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = require("./src/app");
 
 connectToDB()
     // Start server only after DB connects
+    
     app.listen(3000, () => {
         console.log("Server is running on port 3000");
     });
