9a46b42180e5982a3030b23567f56a372a7fa05e
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-04-10 01:44:02 +0530

    done all changes

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema ({
 
 userSchema.pre("save", async function()
 {
-    if (!this.isModifies("password"))
+    if (!this.isModified("password"))
     {
         return ;
 
