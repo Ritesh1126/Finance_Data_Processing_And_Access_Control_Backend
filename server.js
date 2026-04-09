2b47708be7e0cf4351e391f72605a6b017919c58
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-04-09 23:49:39 +0530

    server file change

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
+require('dotenv').config(); // ✅ FIRST line honi chahiye
+
 console.log("MONGO_URI:", process.env.MONGO_URI);
 console.log("PORT:", process.env.PORT);
-require('dotenv').config(); // Must  
-
 
 const connectToDB = require("./src/config/db");
 const app = require("./src/app");
 
-connectToDB()
-    // Start server only after DB connects
-    
-    app.listen(3000, () => {
-        console.log("Server is running on port 3000");
-    });
+connectToDB();
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running on port", process.env.PORT || 3000);
+});
